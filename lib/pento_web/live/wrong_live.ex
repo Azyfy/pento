e560c6a192d52c5b0b796920b2281df86f2879d3
Azryel <AzryelShroud@gmail.com>
2022-04-29 00:30:15 +0200

    Add features to WrongLive (1:yt)

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,35 +1,83 @@
 defmodule PentoWeb.WrongLive do
   use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
 
+  @numbers_to_guess 1..10
+
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    random_number = Enum.random(@numbers_to_guess)
+
+    {:ok, assign(
+                  socket,
+                  score: 0,
+                  message: "Make a guess:",
+                  guess_number: random_number,
+                  numbers_to_guess: @numbers_to_guess,
+                  win: false,
+                  time: time()
+                )}
   end
 
   def render(assigns) do
     ~H"""
       <h1>Your score: <%= @score %></h1>
+      <p>
+        It's <%= @time %>
+      </p>
       <h2>
         <%= @message %>
-        It's <%= time() %>
       </h2>
       <h2>
-        <%= for n <- 1..10 do %>
+        <%= for n <- @numbers_to_guess do %>
           <a href="#" phx-click="guess" phx-value-number={n} ><%= n %></a>
         <% end %>
       </h2>
+      <%= if @win do%>
+        <button phx-click="reset" > Reset </button>
+      <% end %>
     """
   end
 
-  def handle_event("guess", %{"number" => guess}=data, socket) do
-    message = "Your guess: #{guess}. Wrong. Guess again."
-    score = socket.assigns.score - 1
+  def handle_event("guess", %{"number" => guess}=_data, socket) do
+    guess = guess |> String.to_integer()
+    check = guess == socket.assigns.guess_number
+    random_number = Enum.random(@numbers_to_guess)
+
+    message = if check
+                do
+                  "Your guess: #{guess}. Correct. You win."
+                else
+                  "Your guess: #{guess}. Wrong. Guess again."
+                end
+    score = if check
+                do
+                  socket.assigns.score + 1
+                else
+                  socket.assigns.score - 1
+                end
 
     {
       :noreply,
       assign(
         socket,
         message: message,
-        score: score
+        score: score,
+        guess_number: random_number,
+        win: check == true,
+        time: time()
+      )
+    }
+  end
+
+  def handle_event("reset", _, socket) do
+    {
+      :noreply,
+      assign(
+        socket,
+        score: 0,
+        message: "Game reset. Your guess:",
+        guess_number: Enum.random(@numbers_to_guess),
+        win: false,
+        time: time()
       )
     }
   end
