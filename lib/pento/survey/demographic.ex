c6fe9b0e70f362108ae5740fc4f7dfc13e0d74af
Azryel <AzryelShroud@gmail.com>
2022-05-07 23:23:15 +0200

    Survey context
    VScode shows warnings, every console output did pass

--- lib/pento/survey/demographic.ex
+++ lib/pento/survey/demographic.ex
@@ -5,7 +5,8 @@ defmodule Pento.Survey.Demographic do
   schema "demographics" do
     field :gender, :string
     field :year_of_birth, :integer
-    field :user_id, :id
+    # field :user_id, :id
+    belongs_to :user, User
 
     timestamps()
   end
@@ -13,8 +14,13 @@ defmodule Pento.Survey.Demographic do
   @doc false
   def changeset(demographic, attrs) do
     demographic
-    |> cast(attrs, [:gender, :year_of_birth])
-    |> validate_required([:gender, :year_of_birth])
+    |> cast(attrs, [:gender, :year_of_birth, :user_id])
+    |> validate_required([:gender, :year_of_birth, :user_id])
+    |> validate_inclusion(
+      :gender,
+      ["male", "female", "prefer not to say"]
+      )
+      |> validate_inclusion(:year_of_birth, 1900..2022)
     |> unique_constraint(:user_id)
   end
 end
