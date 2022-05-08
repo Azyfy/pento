4cc5b471a13972188cea9a403e97eec2a6ebb15a
Azryel <AzryelShroud@gmail.com>
2022-05-08 14:42:14 +0200

    Survey liveview

--- lib/pento_web/live/user_auth_live.ex
+++ lib/pento_web/live/user_auth_live.ex
@@ -1,19 +1,18 @@
 defmodule PentoWeb.UserAuthLive do
-    import Phoenix.LiveView
+  import Phoenix.LiveView
+  alias Pento.Accounts
 
-    alias Pento.Accounts
+  def on_mount(_, _params, %{"user_token" => user_token} = _session, socket) do
+    IO.puts "Assign User with socket.private:"
+IO.inspect socket.private
 
-    def on_mount(_, _params, %{"user_token" => user_token}, socket) do
-      user = Accounts.get_user_by_session_token(user_token)
-      socket =
-        socket
-        |> assign(:current_user, user)
-      if socket.assigns.current_user do
-        {:cont, socket}
-      else
-        {:halt, redirect(socket, to: "/login")}
-      end
-
+      socket = assign_new(socket, :current_user, fn ->
+          Accounts.get_user_by_session_token(user_token)
+          end)
+    if socket.assigns.current_user do
+      {:cont, socket}
+    else
+      {:halt, redirect(socket, to: "/login")}
     end
-
+  end
 end
