a3f210eb01df4dd9a1362a806c5da9eb65274abf
Azryel <AzryelShroud@gmail.com>
2022-05-08 14:50:01 +0200

    Render component with slots (6)

--- lib/pento_web/live/survey_live/component.ex
+++ lib/pento_web/live/survey_live/component.ex
@@ -4,8 +4,11 @@ defmodule PentoWeb.SurveyLive.Component do
   def hero(assigns) do
     ~H"""
     <h2>
-    content: <%= @content %>
+      content: <%= @content %>
     </h2>
+    <h3>
+      slot: <%= render_slot(@inner_block) %>
+    </h3>
     """
   end
 end
